3bfcdb323a72f573adf70ce562af4861efa42c53
ikamrava <kamrava.iman@outlook.com>
2023-03-21 17:55:07 +0000

    initial commit

--- src/component/EmployeesList.jsx
+++ src/component/EmployeesList.jsx
@@ -1,20 +1,49 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Axio from "axios"
 import Table from './Table'
 
 function EmployeesList() {
     const [employeeList,setEmployeeList] = useState([]) 
+    const [showTable,setShowTable] = useState(false)
+    const [loading,setLoading] = useState(false)
+
+    function deletekHandler(id){
+      setLoading(true)
+      Axio.post("http://localhost:3001/delete",{id}).then((res)=>{
+        console.log(res)
+    })
+      setLoading(false)
+      console.log(id)
+      setEmployeeList( prev => {
+        prev.filter(item => item.id != id)
+      })
+      setShowTable(false)
+    
+}
 
 function clickHandler(){
+  console.log("first")
+  setLoading(true)
+
     Axio.get("https://protected-fortress-49537.herokuapp.com/employees").then((res)=>{
         setEmployeeList(res.data)
-        console.log(employeeList)
+        setShowTable(!showTable)
+       
     })
+    setLoading(false)
+    
 }
+
+
+
+
+
   return (
     <div className=' mx-auto max-w-6xl flex flex-col gap-5'>
      <button className=' bg-slate-800 rounded-lg p-3 text-white w-96 mx-auto' onClick={clickHandler}>Show Employees</button>
-      {employeeList && <Table data = {employeeList} /> }
+     {loading ? ".....Loading" : employeeList && showTable && <Table data = {employeeList}  deletekHandler={deletekHandler} />}
+
+      
     </div>
   )
 }
--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -1,11 +1,10 @@
 
 
-function Table({data}) {
 
-   
+function Table({data ,deletekHandler}) {
+
     const allData = data.map(item=>(
-      
-        <tr className='grid grid-cols-7 align-middle' key={item.id}>
+        <tr className='grid grid-cols-8 align-middle' key={item.id} >
           <td className=' text-center border-2 border-cyan-400'>{item.id}</td>
           <td className=' text-center border-2 border-cyan-400'>{item.f_name}</td>
           <td className=' text-center border-2 border-cyan-400'>{item.l_name}</td>
@@ -13,28 +12,38 @@ function Table({data}) {
           <td className=' text-center border-2 border-cyan-400'>{item.country}</td>
           <td className=' text-center border-2 border-cyan-400'>{item.position}</td>
           <td className=' text-center border-2 border-cyan-400'>{item.wage}</td>
+          <td><button onClick={()=>deletekHandler(item.id)}>Delete</button></td>
       </tr>
+      
      
     ))
       
  
       
   return (
+    
     <table className=' w-[100%] border-2 border-cyan-400'>
-      <thead className=' border-2 border-cyan-400 grid grid-cols-7 align-middle text-center'>
-        <td className='border-2 border-cyan-400'>Employee ID</td>
-        <td className='border-2 border-cyan-400'>First Name</td>
-        <td className='border-2 border-cyan-400'>Last Name</td>
-        <td className='border-2 border-cyan-400'>Age</td>
-        <td className='border-2 border-cyan-400'>Country</td>
-        <td className='border-2 border-cyan-400'>Position</td>
-        <td className='border-2 border-cyan-400'>Wage</td>
-      </thead>
+      {allData.length > 0 && 
+      <thead >
+        <tr className=' border-2 border-cyan-400 grid grid-cols-8 align-middle text-center'>
+          <th className='border-2 border-cyan-400'>Employee ID</th>
+          <th className='border-2 border-cyan-400'>First Name</th>
+          <th className='border-2 border-cyan-400'>Last Name</th>
+          <th className='border-2 border-cyan-400'>Age</th>
+          <th className='border-2 border-cyan-400'>Country</th>
+          <th className='border-2 border-cyan-400'>Position</th>
+          <th className='border-2 border-cyan-400'>Wage</th>
+        </tr>
+  
+    </thead>
+      }
       <tbody className=' border-2 border-cyan-400 '>
         {allData}
+        
       </tbody>
      
       </table>
+    
 
   )
 }
